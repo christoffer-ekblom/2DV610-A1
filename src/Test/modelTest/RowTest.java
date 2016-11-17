96e877f7b816697c416fe53dbe83ffe9ffe5e88d
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-11-17 16:43:08 +0100

    Refactoring RowTest

--- src/Test/modelTest/RowTest.java
+++ src/Test/modelTest/RowTest.java
@@ -2,6 +2,7 @@ package Test.modelTest;
 
 import Mastermind.model.KeyPeg;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.*;
 
@@ -9,36 +10,41 @@ import Mastermind.model.SymbolPeg;
 import Mastermind.model.Row;
 
 public class RowTest {
-    @Test
-    public void shouldReturnSameListOfSymbolPegsAsWeCalledFromConstructor() {
-        List<SymbolPeg> expected = new LinkedList<SymbolPeg>();
-        expected.add(SymbolPeg.King);
-        expected.add(SymbolPeg.Queen);
-        expected.add(SymbolPeg.Diamond);
-        expected.add(SymbolPeg.Heart);
+    Row row;
+    List<SymbolPeg> symbolPegs;
+    List<KeyPeg> keyPegs;
+
+    @Before
+    public void setUp() {
+        List<SymbolPeg> symbolPegs = new LinkedList<SymbolPeg>();
+        symbolPegs.add(SymbolPeg.King);
+        symbolPegs.add(SymbolPeg.Queen);
+        symbolPegs.add(SymbolPeg.Diamond);
+        symbolPegs.add(SymbolPeg.Heart);
+
+        this.symbolPegs = symbolPegs;
 
         List<KeyPeg> keyPegs = new LinkedList<KeyPeg>();
+        keyPegs.add(KeyPeg.Black);
+        keyPegs.add(KeyPeg.Black);
+        keyPegs.add(KeyPeg.White);
 
-        Row row = new Row(expected, keyPegs);
+        this.keyPegs = keyPegs;
 
+        this.row = new Row(symbolPegs, keyPegs);
+    }
+
+    @Test
+    public void shouldReturnSameListOfSymbolPegsAsWeCalledFromConstructor() {
+        List<SymbolPeg> expected = this.symbolPegs;
         List<SymbolPeg> actual = row.getSymbolPegs();
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturnSameListOfKeyPegsAsWeCalledFromConstructor() {
-        List<SymbolPeg> symbolPegs = new LinkedList<SymbolPeg>();
-
-        List<KeyPeg> expected = new LinkedList<KeyPeg>();
-        expected.add(KeyPeg.Black);
-        expected.add(KeyPeg.Black);
-        expected.add(KeyPeg.White);
-
-        Row row = new Row(symbolPegs, expected);
-
+        List<KeyPeg> expected = this.keyPegs;
         List<KeyPeg> actual = row.getKeyPegs();
-
         Assert.assertEquals(expected, actual);
     }
 }
