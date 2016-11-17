4a9320d2c3e299409afd929a8ab37299c555ac83
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2016-11-17 14:40:08 +0100

    REFACTOR view.shouldShowMainMenu

--- src/Mastermind/view/MainView.java
+++ src/Mastermind/view/MainView.java
@@ -6,7 +6,7 @@ public class MainView implements IView {
 
     private PrintStream out;
 
-    public final static String MENU = "MENU";
+    public final static String MENU = "Mastermind Main Menu\n1. Start new game\n2. Settings\n3. Quit";
 
     public MainView(PrintStream output) {
         this.out = output;
