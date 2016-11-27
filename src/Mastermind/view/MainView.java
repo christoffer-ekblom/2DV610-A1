5b0b3bd268df0f6dadaf2ad61ff19551f52eff49
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2016-11-27 13:38:22 +0100

    REFACTOR: Removed Settings menu choice

--- src/Mastermind/view/MainView.java
+++ src/Mastermind/view/MainView.java
@@ -6,7 +6,7 @@ public class MainView {
 
     private PrintStream out;
 
-    public final static String MENU = "Mastermind Main Menu\n1. Start new game\n2. Settings\n3. Quit";
+    public final static String MENU = "Mastermind Main Menu\n1. Start new game\n2. Quit";
     public final static String QUIT = "Thank you for playing Mastermind and welcome back anytime";
 
     public MainView(PrintStream output) {
