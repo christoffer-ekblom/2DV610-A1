24427ea137a0dfe2c1cedb887d506e8e9f929d25
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2016-11-27 15:57:19 +0100

    GREEN: Changed behaviour when Board is instantiated

--- src/Mastermind/model/Board.java
+++ src/Mastermind/model/Board.java
@@ -1,5 +1,6 @@
 package Mastermind.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +12,7 @@ public class Board {
     private List<Row> guessHistory;
 
     public Board() {
-        guessHistory = Arrays.asList(new Row[DEFAULT_TABLE_LENGTH]);
+        guessHistory = new ArrayList<>();
     }
 
     public List<Row> getGuessHistory() {
