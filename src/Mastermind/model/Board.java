7b6d95ccec62ec9bdca519a86f07efdc6ee99e9d
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-11-30 15:06:18 +0100

    GREEN: Should get secret code from opponent

--- src/Mastermind/model/Board.java
+++ src/Mastermind/model/Board.java
@@ -24,8 +24,6 @@ public class Board {
     }
 
     public Row getSecretCodeFromOpponent(Opponent opponent) throws Exception {
-        List<SymbolPeg> code = new LinkedList<>();
-        code.add(SymbolPeg.Club);
-        return new Row(code);
+        return opponent.getSecretCodeAndChangeIt();
     }
 }
